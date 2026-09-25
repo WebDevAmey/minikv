65514fe2798889c97120958a2e2ada8548da4641
Amey Tarmale <ameytarmale7@gmail.com>
2026-09-25 13:30:14 +0530

    add client implementation for KV server connection and command handling

---
+++ src/client.ts
@@ -0,0 +1,42 @@
+import net from "node:net";
+import readline from "node:readline";
+
+const client = net.createConnection(
+    { port: 4000 },
+    () => {
+        console.log("Connected to KV server");
+        console.log("Commands: PUT key value | GET key | DELETE key");
+        rl.prompt();
+    }
+);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> "
+});
+
+rl.on("line", (line) => {
+    const command = line.trim();
+
+    if (!command) {
+        rl.prompt();=
+        return;
+    }
+
+    client.write(command + "\n");
+});
+
+client.on("data", (data) => {
+    console.log(data.toString().trim());
+    rl.prompt();
+});
+
+client.on("error", (err) => {
+    console.log("Error:", err.message);
+});
+
+client.on("close", () => {
+    console.log("Disconnected from server");
+    process.exit(0);
+});
